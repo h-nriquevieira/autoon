bb317152d1e8b43d1b1d6b9bb202d1029781f7fe
h-nriquevieira <h.nriquevieira@gmail.com>
2022-08-20 19:44:21 -0300

    Implement search functionality

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Banner from '../Banner/Banner';
 import Card from '../Card/Card';
 import data from '../../data.json'
 import './Catalog.css'
 
 function Catalog() {
-  const carNodes = data.map(car => <Card info={car} />)
+  const [filter, setFilter] = useState('')
+
+  const filteredData = data.filter(car => {
+    let model = car.model.toLowerCase();
+    return model.includes(filter)
+  })
+
+  
+  const carNodes = filter.length > 0 ? 
+                      filteredData.map(car => <Card info={car} />) 
+                    : data.map(car => <Card info={car} />)
+
+  function handleChange(e) {
+    setFilter(e.target.value)
+    console.log(filter)
+  }
 
   return (
       <main className='catalog'>
         <Banner />
         <div className="cards-container">
             <p className='cards-container--title'>Confira nosso catálogo</p>
+            <input type="text" placeholder='Pesquisar' value={filter} onChange={(e) => handleChange(e)} />
             <div className="cards-container--inner">
                 {carNodes}
             </div>
